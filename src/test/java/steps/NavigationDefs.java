3255d5f4e58946bb85fb86f847f7e871052efa3d
linuxcat <sarndeepnijjar@googlemail.com>
2013-07-21 14:03:00 +0100

    SN: removed pending and added some code to test reporting

--- src/test/java/steps/NavigationDefs.java
+++ src/test/java/steps/NavigationDefs.java
@@ -4,6 +4,9 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.runtime.PendingException;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created with IntelliJ IDEA.
  * User: sarndeep
@@ -15,10 +18,16 @@ public class NavigationDefs {
     @Given("^I am viewing sunday times homepage$")
     public void I_am_viewing_sunday_times_homepage() throws Throwable {
         // Express the Regexp above with the code you wish you had
+        System.out.println("homepage");
+
     }
 
-    @Then("^I should see a top level nav with <nav_item>$")
-    public void I_should_see_a_top_level_nav_with_nav_item() throws Throwable {
+    @Then("^I should see a top level nav with (.*)$")
+    public void I_should_see_a_top_level_nav_with_nav_item(String nav_item) throws Throwable {
         // Express the Regexp above with the code you wish you had
+        assertEquals(nav_item, nav_item);
+        System.out.println("--------------------------------------");
+        System.out.println(nav_item);
+
     }
 }
