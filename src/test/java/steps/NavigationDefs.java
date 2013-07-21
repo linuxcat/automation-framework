2beb0caed616d11c8b7bc3d20b1c7f6f107d13b8
linuxcat <sarndeepnijjar@googlemail.com>
2013-07-21 16:11:21 +0100

    SN: updated test

--- src/test/java/steps/NavigationDefs.java
+++ src/test/java/steps/NavigationDefs.java
@@ -25,9 +25,7 @@ public class NavigationDefs {
     @Then("^I should see a top level nav with (.*)$")
     public void I_should_see_a_top_level_nav_with_nav_item(String nav_item) throws Throwable {
         // Express the Regexp above with the code you wish you had
-        System.out.println("--------------------------------------");
-        System.out.println(nav_item);
-        assertEquals(nav_item, nav_item);
+        assertEquals(nav_item, "hello");
 
 
     }
