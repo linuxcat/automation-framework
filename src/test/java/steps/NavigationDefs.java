9f251b293986a94926d0ce6525d349e63a11fcb5
linuxcat <sarndeepnijjar@googlemail.com>
2013-07-21 13:30:38 +0100

    SN: removed pending

--- src/test/java/steps/NavigationDefs.java
+++ src/test/java/steps/NavigationDefs.java
@@ -15,12 +15,10 @@ public class NavigationDefs {
     @Given("^I am viewing sunday times homepage$")
     public void I_am_viewing_sunday_times_homepage() throws Throwable {
         // Express the Regexp above with the code you wish you had
-        throw new PendingException();
     }
 
     @Then("^I should see a top level nav with <nav_item>$")
     public void I_should_see_a_top_level_nav_with_nav_item() throws Throwable {
         // Express the Regexp above with the code you wish you had
-        throw new PendingException();
     }
 }
